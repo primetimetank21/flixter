f1fc11441c18c3520195658a147435d43d035d85
Earl Tankard, Jr <theflashtank21@gmail.com>
2021-02-17 01:51:04 -0500

    Finished Flixter Assignment (part 2)

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Earl Tankard, Jr. on 2/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
